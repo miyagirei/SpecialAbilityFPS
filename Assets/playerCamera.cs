4eac4f8553cba8034039f7804844665836816ae1
miyagirei <ra.mu.umy243@gmail.com>
2022-04-27 11:00:57 +0900

    移動のスクリプトとカメラのスクリプトを作成、プレイヤーに配置

---
+++ Assets/playerCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCamera : MonoBehaviour
+{
+    public Transform yAxis;
+    public Transform xAxis;
+    public float xSence;//横の感度
+    public float ySence;//縦の感度
+    public float limitXAxizAngle = 30;//縦の限界角度
+    private Vector3 mXAxiz;
+    void Start(){
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        mXAxiz = xAxis.localEulerAngles;
+    }
+    /// <summary>
+    /// キャラクターのカメラ操作に関するメソッド
+    /// </summary>
+    private void cameraFPS(){
+        float xCamera = Input.GetAxis("Mouse X") * -xSence * Time.deltaTime;
+        yAxis.transform.Rotate(0, -xCamera, 0);
+
+        float yCamera = Input.GetAxis("Mouse Y") * -ySence * Time.deltaTime;
+        var x = mXAxiz.x + yCamera;
+        if (x >= -limitXAxizAngle && x <= limitXAxizAngle){
+            mXAxiz.x = x;
+            xAxis.localEulerAngles = mXAxiz;
+        }
+    }
+    void Update(){
+        cameraFPS();
+    }
+}
---
+++ Assets/playerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour{
+    public float mainSpeed = 3.0f;//移動速度
+    void Start(){
+        
+    }
+    void Update(){
+        characterMove();
+    }
+    /// <summary>
+    /// キャラクターの移動に関するメソッド
+    /// </summary>
+    void characterMove(){
+        Transform trans = transform;
+        transform.position = trans.position;
+        trans.position += trans.TransformDirection(Vector3.forward) * Input.GetAxis("Vertical") * mainSpeed;
+        trans.position += trans.TransformDirection(Vector3.right) * Input.GetAxis("Horizontal") * mainSpeed;
+    }
+}
